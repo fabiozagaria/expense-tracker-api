b42d263ead5128b603d02960eea1e060c2abc5e6
Fabio Zagaria <fabiozagaria@proton.me>
2026-09-11 10:28:25 +0200

    Added Security Bean JWT
    Signed-off-by: Fabio Zagaria <fabiozagaria@proton.me>

--- src/main/java/org/esercizi/expensetrackerapi/security/JwtService.java
+++ src/main/java/org/esercizi/expensetrackerapi/security/JwtService.java
@@ -1,12 +1,15 @@
 package org.esercizi.expensetrackerapi.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -16,11 +19,13 @@ public class JwtService {
     public String getAccessTokenJWT(String username) {
         Instant now = Instant.now();
         JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .issuer("fzdev")
                 .subject(username)
                 .issuedAt(now)
-                .expiresAt(now.plusSeconds(900L))
+                .expiresAt(now.plus(15, ChronoUnit.MINUTES))
                 .build();
-        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(jwtClaimsSet);
+        JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(jwsHeader, jwtClaimsSet);
         return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
 
     }
