21f58451ab47ced996296e600ef3600c42c91437
Fabio Zagaria <fabiozagaria@proton.me>
2026-09-13 12:37:41 +0200

    Added logout
    Signed-off-by: Fabio Zagaria <fabiozagaria@proton.me>

--- src/main/java/org/esercizi/expensetrackerapi/security/SecurityConfig.java
+++ src/main/java/org/esercizi/expensetrackerapi/security/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -47,8 +48,7 @@ public class SecurityConfig {
                                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .csrf(
-                        csrf ->
-                                csrf.disable()
+                        AbstractHttpConfigurer::disable
                 )
                 .oauth2ResourceServer(
                         oauth2 ->
