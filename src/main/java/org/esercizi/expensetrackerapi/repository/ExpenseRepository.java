98f03d0662b012f62445dd8bb6151e1293036d4b
Fabio <fabiozagaria@proton.me>
2026-09-21 13:44:21 +0200

    feat: calculate income expense balance

--- src/main/java/org/esercizi/expensetrackerapi/repository/ExpenseRepository.java
+++ src/main/java/org/esercizi/expensetrackerapi/repository/ExpenseRepository.java
@@ -2,7 +2,10 @@ package org.esercizi.expensetrackerapi.repository;
 
 import org.esercizi.expensetrackerapi.model.expense.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +14,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long>{
     List<Expense> findAllByOwnerUsername(String username);
 
     Optional<Expense> findByIdAndOwnerUsername(long id, String username);
+
+    @Query("select coalesce(sum(e.amount), 0) from Expense e where e.owner.username = :username")
+    BigDecimal sumAmountByOwnerUsername(@Param("username") String username);
 }
